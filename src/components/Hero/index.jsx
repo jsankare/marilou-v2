142377bd8c39db05a1fccba2c1211cd0495eb500
jordan sankare <jordan.sankare@gmail.com>
2023-07-28 10:58:39 +0200

    setup gh-pages

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Container = styled.div`
-  background-image: url('/images/dogs_near_water.jpg');
-  background-repeat: no-repeat;
-  background-size: 100% auto;
-  background-position: center;
-  filter: brightness(80%);
-  width: 100%;
-  height: 570px;
-  justify-content: space-between;
-  display: flex;
-  align-items: center;
-`;
+const Container = styled.section`
+    position: relative;
+    width: 100%;
+`
+
+const Background = styled.img`
+    width: 100%;
+    height: 570px;
+    object-fit: cover;
+    position: absolute;
+    z-index: -10;
+    filter: brightness(0.8);
+`
 
 const Content = styled.div`
-  color: white;
-  width: 100%;
-  max-width: 800px;
-  display: flex;
-  flex-direction: column;
-  gap: 50px;
-  margin-left: 5%;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 570px;
+    gap: 50px;
+    padding: 0 100px;
 `
 
 const Title = styled.h1`
@@ -35,14 +36,14 @@ const Subtitle = styled.h2`
 
 function Hero() {
     return (
-      <Container>
-        <Content>
-            <Title>Garde animaux Seine-et-marne (77)</Title>
-            <Subtitle>Prenez soin de votre animal de compagnie en Seine-et-Marne avec Marilou - Services de garde d'animaux de qualité pour votre tranquillité d'esprit.</Subtitle>
-        </Content>
-      </Container>
+        <Container>
+            <Background src="/images/dogs_near_water.jpg" />
+            <Content>
+                <Title>Garde animaux Seine-et-marne (77)</Title>
+                <Subtitle>Prenez soin de votre animal de compagnie en Seine-et-Marne avec Marilou - Services de garde d'animaux de qualité pour votre tranquillité d'esprit.</Subtitle>
+            </Content>
+        </Container>
     );
-  }
-  
+}
 
-export default Hero
+export default Hero;
